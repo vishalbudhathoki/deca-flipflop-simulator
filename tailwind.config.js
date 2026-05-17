d89f95d49ef074f570eef59722683d5d94f0c01f
Vishal <vishalbudhathoki8699@gmail.com>
2026-05-18 02:02:18 +0530

    refactor: reorganize project structure, move simulators and assets to src/, fix tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./index.html", "./src/simulators/**/*.html"],
   theme: {
     extend: {},
   },
